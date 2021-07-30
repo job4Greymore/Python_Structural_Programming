30661395be1cdbe84d0908dff99b349a91119248
job4Greymore <50741794+job4Greymore@users.noreply.github.com>
2021-07-30 21:53:24 +0800

    Commit 1

---
+++ Insert Statement.py
@@ -0,0 +1,4 @@
+#Insert Statement
+print("Test 1")
+name = input("Enter friend's name: ")
+print("Hello", name,"!") #prints greeting + friend's name.
---
+++ Variable.py
@@ -0,0 +1,24 @@
+# Variable assginment
+print("Test 1")
+name = "Alan"
+score = 5
+
+print(name, "scored", score, "points")
+
+#Variable assignment 2 
+print("Test 2")
+birthYear = 2000
+currentYear = 2021
+age = currentYear - birthYear
+
+print("I am ", age, "this year")
+
+#Simultatneous Assignment Statement
+print("Test 3")
+
+x, y = 5, 2
+sum , diff = x+y, x-y
+x, y = y, x #swap values x = 2, y = 5
+
+print(sum, diff)
+print(x, y)

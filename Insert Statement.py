219e5d00a0b2dcef33b34ff1825acca5071b88e3
job4Greymore <50741794+job4Greymore@users.noreply.github.com>
2021-07-30 20:51:54 +0800

    commit

--- Insert Statement.py
+++
@@ -1,4 +0,0 @@
-#Insert Statement
-print("Test 1")
-name = input("Enter friend's name: ")
-print("Hello", name,"!") #prints greeting + friend's name.
--- Output Statement.py
+++
@@ -1,9 +0,0 @@
-# Default print
-print("Hello World")
-print(2+3)
-print("2+3 =", 2+3)
-
-# print in a line without breakline by using the ", end = "" "
-print("Hello World", end = " ")
-print(2+3 , end = " ")
-print("2+3 =", 2+3 )
